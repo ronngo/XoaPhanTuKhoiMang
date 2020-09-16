928edc03b02ffd7636017fc92bc85c077ad9b380
ronngo <70925598+ronngo@users.noreply.github.com>
2020-09-16 10:57:35 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace XoaPhanTuKhoiMang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {4, 6, 7, 8, 1};
+            int n;
+            Console.Write("Mang ");
+            HienMang(array, array.Length);
+            Console.WriteLine("Nhap phan tu can xoa trong mang");
+            n = Convert.ToInt32(Console.ReadLine());
+            Xoa(array, n);
+            Console.WriteLine("Mang sau khi xoa la: ");
+            HienMang(array, array.Length - 1);
+        }
+
+        static bool Xoa(int[] a, int n)
+        {
+            bool found = false;
+            for (int i = 0; i < a.Length; ++i)
+            {
+                if (found)
+                {
+                    a[i - 1] = a[i];
+                }
+                else if (a[i] == n)
+                {
+                    int x = i;
+                    Console.WriteLine($"Vi tri cua phan tu {n} la : {x} ");
+                    found = true;
+                }
+            }
+            return found;
+        }
+
+        static void HienMang(int[] a, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
